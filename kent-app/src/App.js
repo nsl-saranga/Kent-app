d7774370c0eba4a3d2d065d360d73014fb039ff5
nsl-saranga <nisal.saranga1234@gmail.com>
2024-05-12 16:04:52 +0530

    added more functionalities in the home page

--- kent-app/src/App.js
+++ kent-app/src/App.js
@@ -1,10 +1,22 @@
-import HomePage from './Pages/HomePage/HomePage'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage/HomePage';
+import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
 
-const App=()=>{
-  return(
-    <HomePage/>
-  )
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div id='page-body'>
+          <Routes>
+            <Route path='/' element={<HomePage/>}></Route>
+            <Route path='/categories/:productId' element={<HomePage/>}></Route>
+            <Route path='*' element={<NotFoundPage/>}></Route>
+          </Routes> 
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
-
+export default App;
--- kent-app/src/Pages/HomePage/HomePage.js
+++ kent-app/src/Pages/HomePage/HomePage.js
@@ -3,28 +3,60 @@ import SideBar from '../../Components/SideBar/SideBar';
 import SelectMenu from '../../Components/SelectMenu/SelectMenu';
 import Card from '../../Components/Card/Card';
 import './HomePage.css'
+import { useParams } from 'react-router-dom';
 
 function HomePage() {
-  return (
-    <>
-      <NavBar/>
-      <div className='page-content'>
-        <SideBar/>
-        <div className='page-column'>
-          <SelectMenu/>
-          <div className='card-container'>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+  const {productId}= useParams()
+  if(productId)
+    {
+      return (
+        <>
+          <NavBar/>
+          <div className='page-content'>
+            <SideBar/>
+            <div className='page-column'>
+              <SelectMenu productId={productId}/>
+              <div className='card-container'>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </>
-  )     
+        </>
+      )   
+      
+    }
+    else
+    {
+      return (
+        <>
+          <NavBar/>
+          <div className='page-content'>
+            <SideBar/>
+            <div className='page-column'>
+              <SelectMenu productId={null}/>
+              <div className='card-container'>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+              </div>
+            </div>
+          </div>
+        </>
+      )   
+      
+
+    }
+    
 }
 
 export default HomePage;
